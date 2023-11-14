6ed41e288e9ee5d954d5030e750428615642e39f
Hazemmohamed123 <hm9702776@gmail.com>
2023-11-13 23:54:57 -0800

    update1

--- _putchar.c
+++ _putchar.c
@@ -2,21 +2,12 @@
 
 /**
  * _putchar - writes the character c to stdout
- * @c: the character to print
+ * @c: The character to print
  *
- * Return: on success 1.
- * on error, -1 us returned, and errno is set appropriately
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(int c)
+int _putchar(char c)
 {
-static int i;
-static char buf[OUTPUT_BUF_SIZE];
-if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
-{
-write(1, buf, i);
-i = 0;
-}
-if (c != BUF_FLUSH)
-buf[i++] = c;
-return (1);
+return (write(1, &c, 1));
 }
